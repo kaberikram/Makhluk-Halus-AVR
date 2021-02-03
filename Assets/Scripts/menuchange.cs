e6de103737810a05373023bd1dd94e2ab7337d34
Ikram Hakim <60850348+kaberikram@users.noreply.github.com>
2021-02-04 01:30:57 +0800

    Updated W Scene Change

---
+++ Assets/Scripts/menuchange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuchange : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("Tutorial_Scene");
+    }
+
+    public void Change2()
+    {
+        SceneManager.LoadScene("LEVEL_01");
+    }
+
+
+
+}
