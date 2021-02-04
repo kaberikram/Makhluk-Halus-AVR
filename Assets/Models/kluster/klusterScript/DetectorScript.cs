a518f76e4217982f2ddb5774fdbe53d59e1f6e7c
arifhilmi1999 <76122335+arifhilmi1999@users.noreply.github.com>
2021-02-04 13:03:40 +0800

    Kluster Added

---
+++ Assets/Models/kluster/klusterScript/DetectorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorScript : MonoBehaviour
+{
+    public GameObject warning;
+    public AudioSource warningSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "vrPlayer")
+        {
+            warning.SetActive(true);
+            StartCoroutine("WaitForSec");
+            warningSound.Play();
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(1);
+        warning.SetActive(false);
+    }
+}
