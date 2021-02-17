958faab1d4463874f00417e744bdc18cfea97135
Ikram Hakim <60850348+kaberikram@users.noreply.github.com>
2021-02-18 02:33:58 +0800

    updated ui

---
+++ Assets/Scripts/HishamPos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HishamPos : MonoBehaviour
+{
+    public Transform[] wayPointList;
+
+    public int currentWayPoint = 0;
+    Transform targetWayPoint;
+
+    public float speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentWayPoint < this.wayPointList.Length)
+        {
+            if (targetWayPoint == null)
+                targetWayPoint = wayPointList[currentWayPoint];
+            walk();
+        }
+    }
+
+    void walk()
+    {
+
+        // move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+        if (transform.position == targetWayPoint.position)
+        {
+            currentWayPoint++;
+            targetWayPoint = wayPointList[currentWayPoint];
+        }
+    }
+}
