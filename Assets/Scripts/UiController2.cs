f2938fae5a2bf6a01b4b402a3d412b5301397982
Ikram Hakim <ikramandaliff@gmail.com>
2021-03-11 15:57:53 +0800

    Updated with Sound lvl1

---
+++ Assets/Scripts/UiController2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController2 : MonoBehaviour
+{
+
+
+    public GameObject m_GotHitScreen;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Virus"))
+        {
+
+            gotHurt();
+
+
+        }
+        if (other.gameObject.CompareTag("VirusSmall"))
+        {
+
+            gotHurt();
+
+
+        }
+    }
+
+
+    public void Update()
+    {
+
+        if (m_GotHitScreen != null)
+        {
+            if (m_GotHitScreen.GetComponent<Image>().color.a > 0)
+            {
+                var color = m_GotHitScreen.GetComponent<Image>().color;
+                color.a -= 0.005f;
+
+                m_GotHitScreen.GetComponent<Image>().color = color;
+
+            }
+        }
+    }
+
+    void gotHurt()
+    {
+        var color = m_GotHitScreen.GetComponent<Image>().color;
+        color.a = 0.6f;
+
+        m_GotHitScreen.GetComponent<Image>().color = color;
+    }
+
+}
